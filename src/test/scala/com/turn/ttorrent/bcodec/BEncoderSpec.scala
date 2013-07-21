8dccb21c8d4311be5081a7a2274bdbde3fe3d9b5
Dan Oxlade <dan.oxlade@gmail.com>
2013-07-21 20:15:44 +0100

    adding more tests and removing html generation

--- src/test/scala/com/turn/ttorrent/bcodec/BEncoderSpec.scala
+++ src/test/scala/com/turn/ttorrent/bcodec/BEncoderSpec.scala
@@ -2,6 +2,7 @@ package com.turn.ttorrent.bcodec
 
 import org.specs2.mutable.Specification
 import java.io.ByteArrayOutputStream
+import java.util
 
 class BEncoderSpec extends Specification {
   "BEncoder" should {
@@ -21,6 +22,31 @@ class BEncoderSpec extends Specification {
       val bencoded = new String(out.toByteArray, "UTF-8")
 
       bencoded mustEqual "i-49e"
+    }
+
+    "encode lists" in {
+      val out: ByteArrayOutputStream = new ByteArrayOutputStream()
+      val list: util.ArrayList[BEValue] = new util.ArrayList[BEValue]()
+      list.add(new BEValue(42))
+      list.add(new BEValue("spam"))
+      BEncoder.bencode(list, out)
+
+      val bencoded = new String(out.toByteArray, "UTF-8")
+
+      bencoded mustEqual "l4:spami42ee"
+    }
+
+    "encode dictionaries" in {
+      val out: ByteArrayOutputStream = new ByteArrayOutputStream()
+
+      val dic = new util.LinkedHashMap[String, BEValue]()
+      dic.put("foo", new BEValue(42))
+      dic.put("bar", new BEValue("spam"))
+      BEncoder.bencode(dic, out)
+
+      val bencoded = new String(out.toByteArray, "UTF-8")
+
+      bencoded mustEqual "d3:bar4:spam3:fooi42ee"
     }
   }
 }
